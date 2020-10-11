51f235ec0e6ae09474e4348a69e9fb9d60bf7231
Jovan <I.Pikula54@gmail.com>
2020-10-11 22:50:22 +0300

    Create fist class

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+      // Comment
+        // Next string deduces "Hello Wordl!" in console
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println("Another string of text in a console");
+        System.out.println("2+2");
+        System.out.println(2+2);
+        System.out.println("2+2="+2+2);
+        System.out.println("2+2="+(2+2));
+        /*System.out.println("2+2="+2+2);
+        System.out.println("2+2="+(2+2));*/
+        System.out.println("2+2="+(2/*+2*/));
+        System.out.println("String 1 \n\tString 2 \n\t\t String 3");
+
+
+
+
+
+
+    }
+}
